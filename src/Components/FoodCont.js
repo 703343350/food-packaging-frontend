9a5cc01911e0add46372af71429a34d78fbe4d61
Vashistha <703343350@genpact.com>
2023-02-21 06:21:33 -0600

    chnages

--- src/Components/FoodCont.js
+++ src/Components/FoodCont.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import "./foodcont.css";
 import FoodBox from "./FoodBox";
 import card from "../img/card1.png";
@@ -10,29 +10,47 @@ import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import food from '../img/bckgrnd.avif'
 import ingred from '../img/ingred.jpg'
-
+import axios from 'axios'
 
 
 
 function FoodCont() {
   const [search,setSearch] = useState(false)
   const [input,setInput]  =useState('')
-  const [data,setData] = useState({ 
-    "recipe": { name: "Chocolate Chip Cookies", 
-    "ingredients": [ "1 cup butter, softened", "1 cup white sugar", "1 cup brown sugar", "2 eggs", "2 teaspoons vanilla extract", "3 cups all-purpose flour", "1 teaspoon baking soda", "2 teaspoons hot water", "1/2 teaspoon salt", "2 cups semisweet chocolate chips", "1 cup chopped walnuts (optional)" ], 
-    "instructions": [ "Preheat oven to 350 degrees F (175 degrees C).", "Cream together the butter, white sugar, and brown sugar until smooth. Beat in the eggs one at a time, then stir in the vanilla. Dissolve baking soda in hot water. Add to batter along with salt. Stir in flour, chocolate chips, and nuts. Drop by large spoonfuls onto ungreased pans.", "Bake for about 10 minutes in the preheated oven, or until edges are nicely browned." ] }, 
-    "restaurants": [ { "name": "The Chocolate Room", "price_range": "₹₹", "menu_items": [ { "name": "Chocolate Chip Cookie", "price": 60 }, { "name": "Double Chocolate Cookie", "price": 70 } ] }, { "name": "Warm Oven", "price_range": "₹₹₹", "menu_items": [ { "name": "Classic Chocolate Chip Cookie", "price": 120 }, { "name": "Triple Chocolate Cookie", "price": 150 } ] }, { "name": "The Donut Baker", "price_range": "₹", "menu_items": [ { "name": "Chocolate Chip Cookie", "price": 35 }, { "name": "Oatmeal Raisin Cookie", "price": 30 } ] } ] })
+  const [name,setName] = useState('')
+  const [data,setData] = useState({})
+  const [ingredients,setIngred] = useState([])
+  const [recipe,setRecipe] = useState([])
+  const [res,setRes] = useState([])
     //console.log("data",data.recipe.ingredients)
-    data.recipe.name = data.recipe.name.toUpperCase();
-    const listItems = data.recipe.ingredients.map((item) => <li>{item}</li>);
-    const instructions = data.recipe.instructions.map((item) => <li>{item}</li>);
-    const restaurants = data.restaurants.map((item)=> <Link to="/foodDetail" state={{name:item.name , price:item.price_range, img:chine}}><FoodBox imgSrc={chine} title={item.name} price={item.price_range} /></Link>)
-    
+   
+   
     function handleSearch(e){
-      setInput('')
-      setSearch(true)
-     
+
+      axios.get('http://localhost:4000/chatgpt?food='+input)
+      .then((res)=>{
+        setData(res.data.data)
+        console.log("sdasd",res)
+       
+      })
+      
+      
     }
+    useEffect(() => {
+      //console.log("data",data,data.recipe,data.hasOwnProperty('recipe'))
+      if(data.hasOwnProperty('recipe')){
+        
+        setIngred(data.ingredients.map((item) => <li>{item}</li>));
+        setRecipe(data.recipe.map((item) => <li>{item}</li>));
+        setRes(data.restaurants.map((item) => <Link to="/foodDetail" state={{img:data.image ,name: item.name, price: item.price }}><FoodBox imgSrc={data.image} title={item.name} price={item.price} /></Link>));
+        
+        setName(input.toUpperCase());
+        setInput('')
+        setSearch(true)
+        console.log("data", data)
+      }
+    }, [data])
+    
     function handleInput(e){
       console.log("input",e.target.value)
       setInput(e.target.value)
@@ -75,16 +93,16 @@ function FoodCont() {
         <br/>
         <div>
           
-          <h3 style={{fontSize:'29px',fontWeight:'800'}}><u>RECIPE: {data.recipe.name}</u></h3><br/>
+          <h3 style={{fontSize:'29px',fontWeight:'800'}}><u>RECIPE: {name}</u></h3><br/>
           <div className="ingredients" style={{backgroundImage:`url(${ingred})`,backgroundRepeat:'no-repeat',backgroundSize:'cover', border:'5px solid #FE555F',padding:'20px',borderRadius:'6px',marginTop:'12px'}}>
           <h4 style={{marginLeft:'44%',marginTop:'-4%',fontSize:'27px',backgroundColor:'white',width:'18%',border:'5px solid #FE555F'}}>INGREDIENTS</h4>
-           <div style={{color:'white'}}>{listItems}</div>
+           <div style={{color:'white'}}>{ingredients}</div>
            </div>
            <br/>
            <div className="ingredients" style={{backgroundImage:`url(${ingred})`,backgroundRepeat:'no-repeat',backgroundSize:'cover', border:'5px solid #FE555F',padding:'20px',borderRadius:'6px',marginTop:'12px'}}>
 
            <h4 style={{marginLeft:'44%',marginTop:'-4%',fontSize:'27px',backgroundColor:'white',width:'20%',border:'5px solid #FE555F'}}>INSTRUCTIONS</h4>
-           <div style={{color:'white'}}>{instructions}</div>
+           <div style={{color:'white'}}>{recipe}</div>
            </div>
            <br/>
 
@@ -94,7 +112,7 @@ function FoodCont() {
             <main>
               
               <br/>
-              {restaurants}
+              {res}
             </main>
           </div>
         </div>}
