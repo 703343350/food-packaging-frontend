b3555aa4f9fc61f3494a49cb5cdcbf0eb80f0112
Vashistha <703343350@genpact.com>
2023-02-22 01:38:15 -0600

    chnages

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
 import { Security } from "@okta/okta-react";
 import Layout from "./Components/Login/Layout";
 
-const oktaAuth = new OktaAuth({ 
-  issuer: process.env.REACT_APP_OKTA_ISSUER, 
-  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
-  redirectUri: window.location.origin + "/login/callback", });
+// const oktaAuth = new OktaAuth({ 
+//   issuer: process.env.REACT_APP_OKTA_ISSUER, 
+//   clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+//   redirectUri: window.location.origin + "/login/callback", });
 
 function App() {
   // const history = useNavigate();
